0556f32b422c756ca02df31be9d8beebb3f7c0d9
Derplord3264 <96126289+Derplord3264@users.noreply.github.com>
2024-10-02 10:48:25 -0400

    Delete download_dataset.py

--- download_dataset.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-def download_dataset():
-    dataset = load_dataset('daily_dialog')
-    dataset.save_to_disk('data/daily_dialog')
-
-if __name__ == "__main__":
-    download_dataset()
